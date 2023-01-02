deddf0dd52ff525d68873031b02961a04fc898b4
Matteo Bonacini <matteo.bonna@gmail.com>
2023-01-02 12:08:17 +0100

    mods

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-mkdir -p ./build/report &&
+mkdir -p ./docs/report/build &&
 cd docs/report &&
 export "$(xargs < .env)" &&
-pdflatex -output-directory=../../build/report ./report.tex;
-bibtex ../../build/report/report.aux;     # Whoever wrote bibtex needs to just stop.
-pdflatex -output-directory=../../build/report ./report.tex && # Yep. Needed for references.
-pdflatex -output-directory=../../build/report ./report.tex;   # Yep. Needed for bibtex.
+pdflatex -output-directory=build ./report.tex;
+bibtex build/report.aux;     # Whoever wrote bibtex needs to just stop.
+pdflatex -output-directory=build ./report.tex && # Yep. Needed for references.
+pdflatex -output-directory=build ./report.tex;   # Yep. Needed for bibtex.
